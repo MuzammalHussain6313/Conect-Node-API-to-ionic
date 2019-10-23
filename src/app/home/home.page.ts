4f33091310109f18840e6dd0ce951c48eef8cae2
MuzammalHussain6313 <muzammal.bsse3310@iiu.edu.pk>
2019-10-23 18:54:41 +0500

    readme file

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {NavController} from '@ionic/angular';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,19 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(public navCtrl: NavController, public http: HttpClient) {
+    this.data = this.http.get('https://jsonplaceholder.typicode.com/posts');
+    this.data.subscribe(data => {
+      this.result = data;
+    });
+  }
 
+  result: any = [];
+  data: Observable<any>;
+  clicked() {
+    this.data = this.http.get('https://jsonplaceholder.typicode.com/posts');
+    this.data.subscribe(data => {
+      console.log(data);
+    });
+  }
 }
